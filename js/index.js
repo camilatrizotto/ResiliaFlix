78dc9e69c99f49c82238fbd24420305213f06899
camilatrizotto <81185222+camilatrizotto@users.noreply.github.com>
2021-05-20 09:43:53 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $('input').keyup((e)=>{
     console.log(e.keyCode)
     if(e.keyCode === 13){
         //window.location.href = 'file:///C:/Users/PC/Desktop/ResiliaFlix/ProjetoFinalFlix/html/pagina2login.html'
-        window.location.href = '../html/pagina2login.html'
+        window.location.href = "../html/pagina2login.html"
     }
 })
 
